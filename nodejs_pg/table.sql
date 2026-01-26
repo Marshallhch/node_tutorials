56db9bfdc88ac7643b862eba014122923795699f
Mashallhch <marshallhch@gmail.com>
2026-01-26 13:14:38 +0900

    node pg setting

---
+++ nodejs_pg/table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE test (
+  id int, -- 숫자형 아이디
+  name varchar(50), -- 50바이트 문자열 이름
+  email varchar(50) -- 50바이트 문자열 이메일
+)
+
+-- 테이블 데이터 입력
+INSERT INTO test VALUES (1, 'marshall', 'mar@naver.com')
+INSERT INTO test VALUES (2, 'jane', 'jane@naver.com')
