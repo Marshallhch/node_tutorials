7133df3d49448ec948108118d6cc848552a5b136
Mashallhch <marshallhch@gmail.com>
2026-01-29 10:22:40 +0900

    sales map load map

--- fullstack_dashboard/front/src/components/dashboard/TargetReality.jsx
+++ fullstack_dashboard/front/src/components/dashboard/TargetReality.jsx
@@ -1,28 +1,34 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { fetchTargetReality } from '../../redux/slices/apiSlice'
-import HeadTitle from './HeadTitle'
-import { BarChart, Bar, XAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts'
-import { TARGET_REALITY_LISTS } from '../../constants/menuList'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchTargetReality } from '../../redux/slices/apiSlice';
+import HeadTitle from './HeadTitle';
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts';
+import { TARGET_REALITY_LISTS } from '../../constants/menuList';
 
-const formatTooltipValue = (value) => `${value} Sales`
+const formatTooltipValue = (value) => `${value} Sales`;
 
 const TargetReality = () => {
-
-  const dispatch = useDispatch()
-  const state = useSelector((state) => state.apis.targetRealityData)
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state.apis.targetRealityData);
 
   useEffect(() => {
-    dispatch(fetchTargetReality())
-  }, [dispatch])
+    dispatch(fetchTargetReality());
+  }, [dispatch]);
 
   // console.log(state)
 
   return (
-    <div className='block-wrap mt-[14px] ml-[14px]'>
+    <div className="block-wrap mt-[14px] ml-[14px]">
       <HeadTitle title="Target vs Reality" />
 
-      <div className='bar-chart w-full h-[180px]'>
+      <div className="bar-chart w-full h-[180px]">
         <ResponsiveContainer width="100%" height="100%">
           <BarChart
             data={state}
@@ -33,9 +39,8 @@ const TargetReality = () => {
               bottom: 5,
             }}
           >
-            
             <XAxis
-              dataKey="month" 
+              dataKey="month"
               tickSize={0}
               axisLine={false}
               tick={({ payload, x, y, dy }) => (
@@ -43,29 +48,31 @@ const TargetReality = () => {
                   x={x}
                   y={y + 25}
                   dy={dy}
-                  textAnchor='middle'
+                  textAnchor="middle"
                   fill="#7b91b0"
                   fontSize={14}
-                >{payload.value}</text>
+                >
+                  {payload.value}
+                </text>
               )}
             />
 
-            <Tooltip 
-              cursor={{fill: 'transparent'}}
+            <Tooltip
+              cursor={{ fill: 'transparent' }}
               formatter={formatTooltipValue}
             />
 
             <Bar
-              dataKey="reality" 
-              fill="#4ab58e" 
+              dataKey="reality"
+              fill="#4ab58e"
               activeBar={false}
               isAnimationActive={false}
               radius={[4, 4, 4, 4]}
               barSize={16}
             />
             <Bar
-              dataKey="target" 
-              fill="#ffcf00" 
+              dataKey="target"
+              fill="#ffcf00"
               activeBar={false}
               isAnimationActive={false}
               radius={[4, 4, 4, 4]}
@@ -75,34 +82,40 @@ const TargetReality = () => {
         </ResponsiveContainer>
       </div>
 
-      <div className='block-foot'>
-        <div className='legend-info mt-4'>
-          {
-            TARGET_REALITY_LISTS.map((item, index) => (
-              <div key={index} className='mt-4 flex items-center justify-between'>
-                <div className='flex items-center gap-x-2.5'>
-                  <div className={`w-10 h-10 rounded-sm flex place-content-center ${index === 0 ? 'bg-[#e2fff3]' : 'bg-[#fff4de]'}`}>
-                    <img src={item.icon} alt={item.title} className="w-6" />
-                  </div>
-                  <div>
-                    <h4 className='text-xs text-[#151d48] dark:text-gray-300'>{item.title}</h4>
-                    <p className='text-[10px] text-[#a5aea3] dark:text-gray-500'>{item.subtitle}</p>
-                  </div>
+      <div className="block-foot">
+        <div className="legend-info mt-4">
+          {TARGET_REALITY_LISTS.map((item, index) => (
+            <div key={index} className="mt-4 flex items-center justify-between">
+              <div className="flex items-center gap-x-2.5">
+                <div
+                  className={`w-10 h-10 rounded-sm flex place-content-center ${index === 0 ? 'bg-[#e2fff3]' : 'bg-[#fff4de]'}`}
+                >
+                  <img src={item.icon} alt={item.title} className="w-6" />
                 </div>
                 <div>
-                  <p className={`font-semibold dark:text-gray-300 ${
-                    index === 0 
-                      ? "text-[#00e096]" 
-                      : "text-[#ffa412]"
-                  }`}>{item.value}</p>
+                  <h4 className="text-xs text-[#151d48] dark:text-gray-300">
+                    {item.title}
+                  </h4>
+                  <p className="text-[10px] text-[#a5aea3] dark:text-gray-500">
+                    {item.subtitle}
+                  </p>
                 </div>
               </div>
-            ))
-          }
+              <div>
+                <p
+                  className={`font-semibold dark:text-gray-300 ${
+                    index === 0 ? 'text-[#00e096]' : 'text-[#ffa412]'
+                  }`}
+                >
+                  {item.value}
+                </p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default TargetReality
+export default TargetReality;
--- fullstack_dashboard/front/src/constants/apiUrls.js
+++ fullstack_dashboard/front/src/constants/apiUrls.js
@@ -1,12 +1,14 @@
-const domain = 'http://localhost:8000'
+const domain = 'http://localhost:8000';
 
-const REST_COUNTRIES_API_URL = 'https://restcountries.com/v3.1/all?fields=name,flags,languages'
+const REST_COUNTRIES_API_URL =
+  'https://restcountries.com/v3.1/all?fields=name,flags,languages';
 
-const GET_VISITORS_API_URL = `${domain}/visitors`
-const GET_REVENUE_API_URL = `${domain}/revenue`
-const GET_CUSTOMERS_API_URL = `${domain}/customers`
-const GET_TARGETREALITY_API_URL = `${domain}/target_reality`
-const GET_TOP_PRODUCTS_API_URL = `${domain}/top_products`
+const GET_VISITORS_API_URL = `${domain}/visitors`;
+const GET_REVENUE_API_URL = `${domain}/revenue`;
+const GET_CUSTOMERS_API_URL = `${domain}/customers`;
+const GET_TARGETREALITY_API_URL = `${domain}/target_reality`;
+const GET_TOP_PRODUCTS_API_URL = `${domain}/top_products`;
+const GET_SALES_MAP_API_URL = `${domain}/sales_map`;
 
 export {
   GET_VISITORS_API_URL,
@@ -14,5 +16,6 @@ export {
   REST_COUNTRIES_API_URL,
   GET_CUSTOMERS_API_URL,
   GET_TARGETREALITY_API_URL,
-  GET_TOP_PRODUCTS_API_URL
-}
+  GET_TOP_PRODUCTS_API_URL,
+  GET_SALES_MAP_API_URL,
+};
