ddbb38a5a21ac7e6c3495faaa8f55b59a0a88590
Marshallhch <marshallhch@gmail.com>
2026-02-19 13:23:54 +0900

    feat: Implement user registration and login functionality with Redux Toolkit.

--- register/front/src/components/Header.jsx
+++ register/front/src/components/Header.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { clearToken } from '../redux/slices/loginSlice';
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.login.user);
+  console.log(user);
+
+  const handleLogout = () => {
+    dispatch(clearToken());
+  };
   return (
     <div>
-      <Link to="/login">LOGIN</Link>
+      <Link to="/login">LOGIN</Link>{' '}
+      <button onClick={handleLogout}>LOGOUT</button>
     </div>
   );
 };
--- register/front/src/utils/apiUrl.js
+++ register/front/src/utils/apiUrl.js
@@ -1,10 +1,10 @@
-let rootPath = '';
+let rootPath = 'http://localhost:8000';
 
-if (process.env.DEV_MODE === 'dev') {
-  rootPath = 'http://localhost:8000';
-} else {
-  rootPath = 'https://your-production-url.com';
-}
+// if (import.meta.env.VITE_DEV_MODE === 'dev') {
+//   rootPath = 'http://localhost:8000';
+// } else {
+//   rootPath = 'https://your-production-url.com';
+// }
 
 const POST_AUTH_URL = `${rootPath}/auth/register`;
 const POST_LOGIN_URL = `${rootPath}/auth/login`;
